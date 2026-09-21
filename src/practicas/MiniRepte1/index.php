3891b34d481464e775cccec6da2895d9ca8a25f9
sergio0al <alonsorojalssergio@fpllefia.com>
2026-09-21 17:33:13 +0200

    ValenciaBarecelona

---
+++ src/practicas/MiniRepte1/index.php
@@ -0,0 +1,43 @@
+<?php
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    
+    <section>
+        <h1 id="title">Valencia C. F. contra Barcelona</h1>    
+        <div id="partidosContainer">
+            <div class="partido">
+                <div class="info">
+                    <div class="equipos">
+                        <img src="//ssl.gstatic.com/onebox/media/sports/logos/optimized/QPbjvDwI_0Wuu4tCS2O6uw_48x48.png">
+                        <p>Valencia C. F.</p>
+                        <img src="//ssl.gstatic.com/onebox/media/sports/logos/optimized/paYnEE8hcrP96neHRNofhQ_48x48.png">
+                        <p>Barcelona</p>
+                    </div>
+                    <div class="resultados">
+                        <p>0</p>
+                        <p>5</p>
+                    </div>
+                <div>
+            </div>
+
+        </div>
+        <div class="partidosContainer">
+
+        </div>
+    </section>
+
+</body>
+</html>
