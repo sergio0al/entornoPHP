347727cf62977fe1a06060d94334819654dd2db6
Sergio Alonso <alonsorojalssergio@fpllefia.com>
2026-09-25 19:32:30 +0200

    M616 - Projecte Intermodular

--- src/practicas/MiniRepte1/index.php
+++ src/practicas/MiniRepte1/index.php
@@ -1,12 +1,18 @@
-<?php
+<?php 
 
+$nombreValencia = "Valencia C. F.";
+$escudoValencia = "//ssl.gstatic.com/onebox/media/sports/logos/optimized/QPbjvDwI_0Wuu4tCS2O6uw_48x48.png";
 
+$nombreBarcelona = "Barcelona";
+$escudoBarcelona = "//ssl.gstatic.com/onebox/media/sports/logos/optimized/paYnEE8hcrP96neHRNofhQ_48x48.png";
+
+$resultadoValencia = "0";
+$resultadoBarcelona = "5";
 
 ?>
 
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,27 +22,68 @@
 <body>
     
     <section>
-        <h1 id="title">Valencia C. F. contra Barcelona</h1>    
+        <h1 id="title"><?= $nombreValencia ?> contra <?= $nombreBarcelona ?></h1>    
+        
         <div id="partidosContainer">
             <div class="partido">
                 <div class="info">
                     <div class="equipos">
-                        <img src="//ssl.gstatic.com/onebox/media/sports/logos/optimized/QPbjvDwI_0Wuu4tCS2O6uw_48x48.png">
-                        <p>Valencia C. F.</p>
-                        <img src="//ssl.gstatic.com/onebox/media/sports/logos/optimized/paYnEE8hcrP96neHRNofhQ_48x48.png">
-                        <p>Barcelona</p>
+                        <img src="<?= $escudoValencia ?>">
+                        <p><?= $nombreValencia ?></p>
+
+                        <img src="<?= $escudoBarcelona ?>">
+                        <p class="ganador"><?= $nombreBarcelona ?></p>
                     </div>
                     <div class="resultados">
-                        <p>0</p>
-                        <p>5</p>
+                        <p><?= $resultadoValencia ?></p>
+                        <p class="ganador"><?= $resultadoBarcelona ?></p>
                     </div>
-                <div>
+                </div>
             </div>
 
-        </div>
-        <div class="partidosContainer">
+            
 
+            <div class="partido">
+                <div class="info">
+                    <div class="equipos">
+                        <img src="<?= $escudoValencia ?>">
+                        <p><?= $nombreValencia ?></p>
+
+                        <img src="<?= $escudoBarcelona ?>">
+                        <p class="ganador"><?= $nombreBarcelona ?></p>
+                    </div>
+                    <div class="resultados">
+                        <p><?= $resultadoValencia ?></p>
+                        <p class="ganador"><?= $resultadoBarcelona ?></p>
+                    </div>
+                </div>
+            </div>
+
+      
+            <div class="partido">
+                <div class="info">
+                    <div class="equipos">
+                        <img src="<?= $escudoBarcelona ?>">
+                        <p><?= $nombreBarcelona ?></p>
+
+                        <img src="<?= $escudoValencia ?>">
+                        <p><?= $nombreValencia ?></p>
+                    </div>
+                    <div class="resultados">
+                        <p></p>
+                        <p></p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="partido"></div>
         </div>
+
+        <div class="footer">
+            <p>Horarios en Hora de Verano de Europa Central</p>
+            <p>Sugerencias</p>
+        </div>
+
     </section>
 
 </body>
